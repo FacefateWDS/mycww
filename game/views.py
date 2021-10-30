2ab376b1ae8cae2ad73da989b2c09b3e76fe77ab
cww <3233446292@qq.com>
2021-10-30 16:18:48 +0800

    update1

--- game/views.py
+++ game/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+def index(request):
+    line =' <h1 style="text-align:center">我的第一个网页！！！</h1>'
+    line2 = '<hr>'
+#    line3 = "<a herf = "/play/">开始</a>"
+    line1 = '<img src ="https://tse1-mm.cn.bing.net/th/id/OIP-C.D9NM7OI_pGDC32kLDVhSAAHaE7?w=286&h=191&c=7&r=0&o=5&dpr=1.5&pid=1.7" width = 800 >'
+    return HttpResponse(line+line1+line2)
